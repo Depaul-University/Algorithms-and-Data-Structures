1359fc8d7fd282cac8a391493f7880d5e39cd6f1
alleaward <Allea_ward@hotmail.com>
2018-07-06 22:29:14 +1000

    Initialized local repo

---
+++ Algorithms-and-Data-Structures/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_and_Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing...");
+            for(var i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
